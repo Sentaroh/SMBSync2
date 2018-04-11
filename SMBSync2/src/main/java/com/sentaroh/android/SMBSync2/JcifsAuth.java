4b87c0f3889f83c7df696035c3c3b4375117a9b1
sentaroh <gm.developer.fhoshino@gmail.com>
2018-04-11 12:34:06 +0900

    コード整理

--- SMBSync2/src/main/java/com/sentaroh/android/SMBSync2/JcifsAuth.java
+++ SMBSync2/src/main/java/com/sentaroh/android/SMBSync2/JcifsAuth.java
@@ -12,6 +12,8 @@ public class JcifsAuth {
 	private String mDomain=null, mUserName=null, mUserPass=null;
 
     /**
+     * SMB1 or SMB2 Constructor
+     *
      * @param smb1
      *            true is use jcifs-1.3.17, false is use jcifs-ng
      * @param domain
@@ -22,8 +24,8 @@ public class JcifsAuth {
      *            A password for user
      * @throws JcifsException
      */
-    public JcifsAuth(boolean smb1, String domain, String user, String pass) {
-        mSmb1=smb1;
+    public JcifsAuth(boolean smb_level, String domain, String user, String pass) {
+        mSmb1=smb_level;
         mDomain=domain;
         mUserName=user;
         mUserPass=pass;
@@ -45,8 +47,8 @@ public class JcifsAuth {
 	}
 
     /**
-     * @param smb1
-     *            true is use jcifs-1.3.17, false is use jcifs-ng
+     * SMB2 Constructor
+     *
      * @param domain
      *            A domain name
      * @param user
@@ -57,53 +59,62 @@ public class JcifsAuth {
      *            true is use IpcSigningEnforced
      * @throws JcifsException
      */
-    public JcifsAuth(boolean smb1, String domain, String user, String pass, boolean ipc_signing_enforced) {
-        mSmb1=smb1;
+    public JcifsAuth(String domain, String user, String pass, boolean ipc_signing_enforced) {
+        mSmb1=JCIFS_FILE_SMB2;
         mDomain=domain;
         mUserName=user;
         mUserPass=pass;
-        if (mSmb1) {
-            mSmb1Auth=new jcifs.smb.NtlmPasswordAuthentication(domain, user, pass);
-        } else {
-            jcifsng.context.BaseContext bc;
-            try {
-                Properties prop=new Properties();
-                if (ipc_signing_enforced) prop.setProperty("jcifs.smb.client.ipcSigningEnforced", "true");
-                else prop.setProperty("jcifs.smb.client.ipcSigningEnforced", "false");
-                prop.setProperty("jcifs.smb.client.minVersion","SMB210");
-                prop.setProperty("jcifs.smb.client.maxVersion","SMB210");
+        jcifsng.context.BaseContext bc;
+        try {
+            Properties prop=new Properties();
+            if (ipc_signing_enforced) prop.setProperty("jcifs.smb.client.ipcSigningEnforced", "true");
+            else prop.setProperty("jcifs.smb.client.ipcSigningEnforced", "false");
+            prop.setProperty("jcifs.smb.client.minVersion","SMB210");
+            prop.setProperty("jcifs.smb.client.maxVersion","SMB210");
 
-                bc = new jcifsng.context.BaseContext(new jcifsng.config.PropertyConfiguration(prop));
-                jcifsng.smb.NtlmPasswordAuthentication creds = new jcifsng.smb.NtlmPasswordAuthentication(bc, domain,user,pass);
-                mSmb2Auth = bc.withCredentials(creds);
-            } catch (jcifsng.CIFSException e) {
-                e.printStackTrace();
-            }
+            bc = new jcifsng.context.BaseContext(new jcifsng.config.PropertyConfiguration(prop));
+            jcifsng.smb.NtlmPasswordAuthentication creds = new jcifsng.smb.NtlmPasswordAuthentication(bc, domain,user,pass);
+            mSmb2Auth = bc.withCredentials(creds);
+        } catch (jcifsng.CIFSException e) {
+            e.printStackTrace();
         }
     }
 
-    public JcifsAuth(boolean smb1, String domain, String user, String pass, boolean ipc_signing_enforced, String min_version, String max_version) {
-        mSmb1=smb1;
+    /**
+     * SMB2 Constructor
+     *
+     * @param domain
+     *            A domain name
+     * @param user
+     *            A user name
+     * @param pass
+     *            A password for user
+     * @param ipc_signing_enforced
+     *            true is use IpcSigningEnforced
+     * @param min_version
+     *            min SMB version ("SMB1" or "SMB210")
+     * @param max_version
+     *            max SMB version ("SMB1" or "SMB210")
+     * @throws JcifsException
+     */
+    public JcifsAuth(String domain, String user, String pass, boolean ipc_signing_enforced, String min_version, String max_version) {
+        mSmb1=JCIFS_FILE_SMB2;
         mDomain=domain;
         mUserName=user;
         mUserPass=pass;
-        if (mSmb1) {
-            mSmb1Auth=new jcifs.smb.NtlmPasswordAuthentication(domain, user, pass);
-        } else {
-            jcifsng.context.BaseContext bc;
-            try {
-                Properties prop=new Properties();
-                if (ipc_signing_enforced) prop.setProperty("jcifs.smb.client.ipcSigningEnforced", "true");
-                else prop.setProperty("jcifs.smb.client.ipcSigningEnforced", "false");
-                prop.setProperty("jcifs.smb.client.minVersion",min_version);
-                prop.setProperty("jcifs.smb.client.maxVersion",max_version);
+        jcifsng.context.BaseContext bc;
+        try {
+            Properties prop=new Properties();
+            if (ipc_signing_enforced) prop.setProperty("jcifs.smb.client.ipcSigningEnforced", "true");
+            else prop.setProperty("jcifs.smb.client.ipcSigningEnforced", "false");
+            prop.setProperty("jcifs.smb.client.minVersion",min_version);
+            prop.setProperty("jcifs.smb.client.maxVersion",max_version);
 
-                bc = new jcifsng.context.BaseContext(new jcifsng.config.PropertyConfiguration(prop));
-                jcifsng.smb.NtlmPasswordAuthentication creds = new jcifsng.smb.NtlmPasswordAuthentication(bc, domain,user,pass);
-                mSmb2Auth = bc.withCredentials(creds);
-            } catch (jcifsng.CIFSException e) {
-                e.printStackTrace();
-            }
+            bc = new jcifsng.context.BaseContext(new jcifsng.config.PropertyConfiguration(prop));
+            jcifsng.smb.NtlmPasswordAuthentication creds = new jcifsng.smb.NtlmPasswordAuthentication(bc, domain,user,pass);
+            mSmb2Auth = bc.withCredentials(creds);
+        } catch (jcifsng.CIFSException e) {
+            e.printStackTrace();
         }
     }
 
