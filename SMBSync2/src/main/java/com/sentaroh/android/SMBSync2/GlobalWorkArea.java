e1d789cad9d6579ea8b9629ece694e7dc54a3c23
sentaroh <gm.developer.fhoshino@gmail.com>
2018-05-07 10:15:12 +0900

    SupportLibraryの更新（V23->V26)

--- SMBSync2/src/main/java/com/sentaroh/android/SMBSync2/GlobalWorkArea.java
+++ SMBSync2/src/main/java/com/sentaroh/android/SMBSync2/GlobalWorkArea.java
@@ -3,7 +3,7 @@ package com.sentaroh.android.SMBSync2;
 import android.content.Context;
 
 public class GlobalWorkArea {
-    static public GlobalParameters gp=null;
+    static private GlobalParameters gp=null;
     static public GlobalParameters getGlobalParameters(Context c) {
         if (gp ==null) {
             gp =new GlobalParameters();
