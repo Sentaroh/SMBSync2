84f03370f6dda193d211a7320b66fbcf885910a6
sentaroh <gm.developer.fhoshino@gmail.com>
2018-06-12 23:57:30 +0900

    コード整理

--- SMBSync2/src/main/java/com/sentaroh/android/SMBSync2/ActivityMain.java
+++ SMBSync2/src/main/java/com/sentaroh/android/SMBSync2/ActivityMain.java
@@ -579,23 +579,23 @@ public class ActivityMain extends AppCompatActivity {
         return mpi;
     }
 
-    private void showBatteryOptimization() {
-        if (Build.VERSION.SDK_INT >= 23) {
-            Intent intent = new Intent();
-            String packageName = mContext.getPackageName();
-            PowerManager pm = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
-            if (pm.isIgnoringBatteryOptimizations(packageName)) {
-                intent.setAction(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS);
-                startActivity(intent);
-                mUtil.addDebugMsg(1, "I", "Invoke battery optimization settings");
-            } else {
-                intent.setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
-                intent.setData(Uri.parse("package:" + packageName));
-                startActivity(intent);
-                mUtil.addDebugMsg(1, "I", "Request ignore battery optimization");
-            }
-        }
-    }
+//    private void showBatteryOptimization() {
+//        if (Build.VERSION.SDK_INT >= 23) {
+//            Intent intent = new Intent();
+//            String packageName = mContext.getPackageName();
+//            PowerManager pm = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
+//            if (pm.isIgnoringBatteryOptimizations(packageName)) {
+//                intent.setAction(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS);
+//                startActivity(intent);
+//                mUtil.addDebugMsg(1, "I", "Invoke battery optimization settings");
+//            } else {
+//                intent.setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
+//                intent.setData(Uri.parse("package:" + packageName));
+//                startActivity(intent);
+//                mUtil.addDebugMsg(1, "I", "Request ignore battery optimization");
+//            }
+//        }
+//    }
 
     class ViewSaveArea {
         public int current_tab_pos = 0;
