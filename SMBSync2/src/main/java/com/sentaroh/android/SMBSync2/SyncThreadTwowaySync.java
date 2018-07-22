95ba03bf17039cf811932a69964f1c6222551d88
sentaroh <gm.developer.fhoshino@gmail.com>
2018-07-23 08:34:48 +0900

    Correct english, Thanks clach04.

--- SMBSync2/src/main/java/com/sentaroh/android/SMBSync2/SyncThreadTwowaySync.java
+++ SMBSync2/src/main/java/com/sentaroh/android/SMBSync2/SyncThreadTwowaySync.java
@@ -1,9 +1,22 @@
 package com.sentaroh.android.SMBSync2;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 public class SyncThreadTwowaySync {
 
@@ -173,11 +186,101 @@ public class SyncThreadTwowaySync {
         return sync_required;
     }
 
+    static public final String TWOWAY_SYNC_FILE_MANAGEMENT_FILE_NAME=".twoway_sync_file_list";
+    static public void saveSyncFileInfoList(SyncThread.SyncThreadWorkArea stwa, SyncTaskItem sti) {
+        if (stwa.newSyncFileInfoList.size()>0) {//merge current list
+            stwa.currSyncFileInfoList.addAll(stwa.newSyncFileInfoList);
+            Collections.sort(stwa.currSyncFileInfoList, new Comparator<SyncFileInfoItem>() {
+                @Override
+                public int compare(SyncFileInfoItem o1, SyncFileInfoItem o2) {
+                    return o1.filePath.compareToIgnoreCase(o2.filePath);
+                }
+            });
+            stwa.newSyncFileInfoList.clear();
+        }
+
+        try {
+            FileOutputStream fos = new FileOutputStream(new File(stwa.gp.settingMgtFileDir+"/"+TWOWAY_SYNC_FILE_MANAGEMENT_FILE_NAME), false);
+            BufferedOutputStream bos = new BufferedOutputStream(fos, 1024 * 1024);
+            ZipOutputStream zos = new ZipOutputStream(bos);
+            ZipEntry ze = new ZipEntry("list.txt");
+            zos.putNextEntry(ze);
+            OutputStreamWriter osw = new OutputStreamWriter(zos, "UTF-8");
+            BufferedWriter bw = new BufferedWriter(osw, 1024 * 1024 * 4);
+
+            StringBuffer pl = new StringBuffer(512);
+
+            for(SyncFileInfoItem item:stwa.currSyncFileInfoList) {
+                pl.setLength(0);
+                pl.append(item.filePath).append("\t")
+                        .append((item.referenced?"1":"0")).append("\t")
+                        .append((item.updated?"1":"0")).append("\t")
+                        .append(String.valueOf(item.syncTime)).append("\t")
+                        .append(String.valueOf(item.fileSize)).append("\t")
+                        .append(String.valueOf(item.fileLastModified)).append("\t");
+                bw.write(pl.toString());
+            }
+
+            bw.flush();
+            bw.close();
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static public void loadSyncFileInfoList(SyncThread.SyncThreadWorkArea stwa, SyncTaskItem sti) {
+        try {
+            FileInputStream fis = new FileInputStream(new File(stwa.gp.settingMgtFileDir+"/"+TWOWAY_SYNC_FILE_MANAGEMENT_FILE_NAME));
+            BufferedInputStream bis = new BufferedInputStream(fis, 1024 * 1024);
+            ZipInputStream zis = new ZipInputStream(bis);
+            zis.getNextEntry();
+            InputStreamReader isr = new InputStreamReader(zis, "UTF-8");
+            BufferedReader br = new BufferedReader(isr, 1024 * 1024 * 4);
+
+            String line = null;
+
+            ArrayList<SyncFileInfoItem> fl=new ArrayList<SyncFileInfoItem>();
+
+            while ((line = br.readLine()) != null) {
+                String[] l_array = line.split("\t");
+                if (l_array != null && l_array.length == 6) {
+                        fl.add(new SyncFileInfoItem(
+                                l_array[0], false, false, Long.valueOf(l_array[3]), Long.valueOf(l_array[4]), Long.valueOf(l_array[5])));
+                }
+            }
+            br.close();
+            stwa.currSyncFileInfoList=fl;
+            stwa.newSyncFileInfoList.clear();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
 
 class SyncFileInfoItem {
+    public boolean referenced=false;
+    public boolean updated=false;
     public long syncTime=0L;
     public String filePath="";
     public long fileSize=0L;
     public long fileLastModified=0L;
+
+    public SyncFileInfoItem() {}
+
+    public SyncFileInfoItem(String fp, boolean ref, boolean upd, long sync_time, long file_size, long last_modified) {
+        filePath=fp;
+        syncTime=sync_time;
+        referenced=ref;
+        updated=upd;
+        fileSize=file_size;
+        fileLastModified=last_modified;
+    }
+
 }
