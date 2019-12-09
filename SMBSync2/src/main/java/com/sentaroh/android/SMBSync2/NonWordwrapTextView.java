89a9b147f2cd7b944ce5d83b9bce3195f890e4c6
Sentaroh <gm.developer.fhoshino@gmail.com>
2019-12-09 13:57:59 +0900

    最新版にリフレッシュ

--- SMBSync2/src/main/java/com/sentaroh/android/SMBSync2/NonWordwrapTextView.java
+++ SMBSync2/src/main/java/com/sentaroh/android/SMBSync2/NonWordwrapTextView.java
@@ -21,6 +21,8 @@ public class NonWordwrapTextView extends TextView {
     private SpannableStringBuilder mSpannableSplitText=null;
     private boolean mWordWrapMode =false;
 
+    private boolean mDebugEnabled=false;
+
     public NonWordwrapTextView(Context context) {
         super(context);
     }
@@ -33,6 +35,8 @@ public class NonWordwrapTextView extends TextView {
         super(context, attrs, defStyle);
     }
 
+    public void setDebugEnable(boolean enabled) {mDebugEnabled=enabled;}
+
     public void setWordWrapEnabled(boolean word_wrap_enabled) {
         mWordWrapMode =word_wrap_enabled;
     }
@@ -43,32 +47,31 @@ public class NonWordwrapTextView extends TextView {
 
     @Override
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-//        log.info("onLayout changed="+changed+", left="+left+", top="+top+", right="+right+", bottom="+bottom);
+        super.onLayout(changed, left, top, right, bottom);
+        if (mDebugEnabled) log.info("onLayout changed="+changed+", left="+left+", top="+top+", right="+right+", bottom="+bottom);
         if (!isWordWrapEnabled()) {
             super.setText(mSpannableSplitText, mOrgBufferType);
+            if (mDebugEnabled) log.info("onLayout setText issued");
         }
     }
 
     @Override
     final protected void onMeasure(int w, int h) {
         TextPaint paint = getPaint();
-        super.onMeasure(w, h);
-//        log.info("onMeasure w="+MeasureSpec.getSize(w)+", h="+MeasureSpec.getSize(h));
+        if (mDebugEnabled) log.info("onMeasure w="+MeasureSpec.getSize(w)+", h="+MeasureSpec.getSize(h));
         if (!isWordWrapEnabled()) {
             mSpannableSplitText=buildSplitText(MeasureSpec.getSize(w), MeasureSpec.getSize(h));
-            float sep_line1=(int)toPixel(getResources(), 3);
-            float sep_line2=(int)toPixel(getResources(), 7);
-//            log.info("ts="+getTextSize()+", paint ts="+paint.getTextSize());
-            int new_h=(int) Math.ceil((paint.getTextSize()+sep_line1)*(float)mSplitTextLineCount+sep_line2);
-//            log.info("onMeasure w="+MeasureSpec.getSize(w)+", h="+MeasureSpec.getSize(h)+", new w="+MeasureSpec.getSize(w)+", new h="+new_h);
-            setMeasuredDimension( MeasureSpec.getSize(w), new_h);
+            super.setText(mSpannableSplitText, mOrgBufferType);
+            super.onMeasure(w, h);
+        } else {
+            super.onMeasure(w, h);
         }
     }
 
     @Override
     final protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-//        log.info("onSizeChanged w="+w+", h="+h+", oldw="+oldw+", oldh="+oldh);
+        if (mDebugEnabled) log.info("onSizeChanged w="+w+", h="+h+", oldw="+oldw+", oldh="+oldh);
     };
 
     @Override
@@ -76,7 +79,8 @@ public class NonWordwrapTextView extends TextView {
         mOrgText = text;
         mOrgBufferType = type;
         super.setText(text, type);
-//        log.info("setText length="+text.length()+", type="+type.toString());
+        requestLayout();
+        if (mDebugEnabled) log.info("setText length="+text.length()+", type="+type.toString()+", text="+text);
     }
 
     @Override
@@ -90,12 +94,11 @@ public class NonWordwrapTextView extends TextView {
     }
 
     public SpannableStringBuilder buildSplitText(int w, int h) {
-        boolean debug=false;
         TextPaint paint = getPaint();
         int wpl =getCompoundPaddingLeft();
         int wpr =getCompoundPaddingRight();
         int width = w - wpl - wpr;
-        if (debug) log.info("buildSplitText width="+width+", w="+w+", wpl="+wpl+", wpr="+wpr+", h="+h+", length="+mOrgText.length());
+        if (mDebugEnabled) log.info("buildSplitText width="+width+", w="+w+", wpl="+wpl+", wpr="+wpr+", h="+h+", length="+mOrgText.length()+", Text="+mOrgText.toString());
 
         SpannableStringBuilder output = null;
         int add_cr_cnt=0;
@@ -105,31 +108,42 @@ public class NonWordwrapTextView extends TextView {
         } else {
             output=new SpannableStringBuilder(mOrgText);
             int start=0;
-            if (debug) log.info("input="+output.toString());
+            if (mDebugEnabled)  {
+                log.info("buildSplitText input length="+output.length());
+                log.info("buildSplitText input  hex ="+ StringUtil.getDumpFormatHexString(mOrgText.toString().getBytes(), 0, mOrgText.toString().getBytes().length));
+            }
+
             while(start<output.length()) {
-                if (debug) log.info("start="+start);
+//                if (mDebugEnabled)  log.info("start="+start);
                 String in_text=output.subSequence(start, output.length()).toString();
                 int cr_pos=in_text.indexOf("\n");
                 if (cr_pos>0) {
                     in_text = output.subSequence(start, start + cr_pos).toString();
                     int nc = paint.breakText(in_text, true, width, null);
-                    if (output.charAt(start + nc) != '\n') output.insert(start + nc, "\n");
+                    if (output.charAt(start + nc) != '\n') {
+                        output.insert(start + nc, "\n");
+//                        log.info("cr inserted1, pos="+(start + nc));
+                    }
                     start = start + nc + 1;
                 } else if (cr_pos==0) {
                     start = start + 1;
                 } else {
                     int nc=paint.breakText(in_text, true, width, null);
-                    output.insert(start+nc, "\n");
-                    start=start+nc+1;
+//                    log.info("start="+start+", nc="+nc);
+//                    log.info("in_text length="+in_text.length()+", text="+in_text);
+                    if (nc<=(output.length()-start-1)) {
+                        output.insert(start+nc, "\n");
+//                        log.info("cr inserted2, pos="+(start + nc)+", output length="+output.length());
+                        start=start+nc+1;
+                    } else {
+                        start=start+nc+1;
+                    }
                 }
             }
             mSplitTextLineCount=output.toString().split("\n").length;
         }
-
-        if (debug) {
-            log.info("buildSplitText Number of Lines="+mSplitTextLineCount+", added_cr/lf_count="+add_cr_cnt);
-            log.info("buildSplitText input  char="+mOrgText.toString());
-            log.info("buildSplitText input  hex ="+ StringUtil.getDumpFormatHexString(mOrgText.toString().getBytes(), 0, mOrgText.toString().getBytes().length));
+        if (mDebugEnabled)  {
+            log.info("buildSplitText Number of Lines="+mSplitTextLineCount);
             log.info("buildSplitText output char="+output.toString());
             log.info("buildSplitText output hex ="+ StringUtil.getDumpFormatHexString(output.toString().getBytes(), 0, output.toString().getBytes().length));
         }
