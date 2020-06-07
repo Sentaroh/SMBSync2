fbabca56c226abd1bf8ab1be1a349f295273207c
PhilZ <phytowardt@gmail.com>
2020-06-07 14:58:29 +0200

    Cleanup showMsg()
    Remove code checks from old version and optimize code

--- SMBSync2/src/main/java/com/sentaroh/android/SMBSync2/SyncThread.java
+++ SMBSync2/src/main/java/com/sentaroh/android/SMBSync2/SyncThread.java
@@ -2039,7 +2039,6 @@ public class SyncThread extends Thread {
                 });
             }
         }
-
         //write to Messages tab: [cat + [space] + full_path + "space" + message] on left column, [type] on right column
         //addLogMsg() displays message on the tabb with fields: cat, type, String... msg: separate different msg fields by " " or a separator for redability
         //argument "type" in addLogMsg() is separate field, displayed in separate column on right of msg, aligned on right border, color themed (file operations...)
@@ -2047,27 +2046,23 @@ public class SyncThread extends Thread {
         //print_msg: remove trailing new line in full_path for readability of the text log file
         //buildPrintMsg(): prints a text file with print_msg, not used for messages display on tab. Text file shown when we click History SyncTask event
         //fields: cat, String... msg: separator " " added between msg fields for redability
-        String lm = "";
-        if (msg.equals("")) lm = full_path;//no message (we should have a type at least), currently full_path is never "" with type. Case lm == "" will also work
-        else lm = full_path.equals("") ? msg : full_path.concat(" ").concat(msg);
-        
 
-        if (task_name.equals("")) {//if also lm == "": we didn't provide any of task_name, message and full_path!
-            stwa.util.addLogMsg(false, true, true, false, cat, task_name, type, full_path, msg);//not currently used (type without task_name)
-            if (stwa.gp.settingWriteSyncResultLog && stwa.syncHistoryWriter != null) {
-                String print_msg = "";
-                if (type.equals("")) print_msg = stwa.util.buildPrintMsg(cat, lm);
-                else print_msg = stwa.util.buildPrintMsg(cat, lm, " ", type);
-                stwa.syncHistoryWriter.println(print_msg);
+        //display message in GUI Messages TAB
+        stwa.util.addLogMsg(false, true, true, false, cat, task_name, type, full_path, msg);
+
+        //buildPrintMsg(): print the sync log text file, shown if we click the Sync event in History TAB
+        String lm = "";
+        String[] print_msg_list = {task_name, full_path, msg, type};
+        for (String msg_part : print_msg_list) {
+            if (!msg_part.equals("")) {
+                if (lm.equals("")) lm = msg_part;
+                else lm = lm.concat(" ").concat(msg_part);
             }
-        } else {
-            stwa.util.addLogMsg(false, true, true, true, cat, task_name, type, full_path, msg);
-            if (stwa.gp.settingWriteSyncResultLog && stwa.syncHistoryWriter != null) {
-                String print_msg = "";
-                if (type.equals("")) print_msg = stwa.util.buildPrintMsg(cat, task_name, " ", lm);
-                else print_msg = stwa.util.buildPrintMsg(cat, task_name, " ", lm, " ", type);
-                stwa.syncHistoryWriter.println(print_msg);
-            }
+        }
+
+        if (stwa.gp.settingWriteSyncResultLog && stwa.syncHistoryWriter != null) {
+            String print_msg = stwa.util.buildPrintMsg(cat, lm);
+            stwa.syncHistoryWriter.println(print_msg);
         }
     }
 
