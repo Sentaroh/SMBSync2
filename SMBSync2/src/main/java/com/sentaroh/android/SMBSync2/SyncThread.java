6138bded9654bc7faa80ca6a6a80e6620d715983
PhilZ <phytowardt@gmail.com>
2020-06-09 16:54:26 +0200

    Exclude filter use friendly syntax
    Check details in issue comments please

--- SMBSync2/src/main/java/com/sentaroh/android/SMBSync2/SyncThread.java
+++ SMBSync2/src/main/java/com/sentaroh/android/SMBSync2/SyncThread.java
@@ -2943,7 +2943,19 @@ public class SyncThread extends Thread {
             } else {
                 if (stwa.dirExcludeFilterPatternList.size()!=0) {
                     for (int i = 0; i < stwa.dirExcludeFilterPatternList.size(); i++) {
-                        mt = stwa.dirExcludeFilterPatternList.get(i).matcher(n_dir);
+                        Pattern filter_pattern = stwa.dirExcludeFilterPatternList.get(i);
+                        String new_filter_dir = n_dir;
+                        if (filter_pattern.toString().startsWith("(^/") || filter_pattern.toString().startsWith("(.*/")) {  // filter==*/cache/* -> excludes master_dir/cache and master_dir/subdir/cache
+                            if (!n_dir.startsWith("/")) new_filter_dir = "/" + n_dir;                             // filter==/cache or filter==cache, only excludes master_dir/cache while master_dir/subdir/cache is included
+                        }
+                        if (!filter_pattern.toString().endsWith("/)") && !filter_pattern.toString().endsWith(".*)")) {// fix above case: filter==/cache -> exclude master/cache/*
+                            String new_pattern_str = filter_pattern.toString();
+                            new_pattern_str = new_pattern_str.substring(0, new_pattern_str.length() - 1) + "/)";
+                            filter_pattern = Pattern.compile(new_pattern_str, Pattern.CASE_INSENSITIVE | Pattern.MULTILINE);
+                        }
+                        if (stwa.gp.settingDebugLevel >= 2)
+                            stwa.util.addDebugMsg(2, "I", "exclude dir by name filter_pattern=" + filter_pattern + " new_filter_dir=" + new_filter_dir);
+                        mt = filter_pattern.matcher(new_filter_dir);
                         if (mt.find()) {
                             if (stwa.currentSTI.isSyncOptionUseExtendedDirectoryFilter1()) {
                                 Pattern[] exc = new Pattern[0];
@@ -3223,7 +3235,19 @@ public class SyncThread extends Thread {
                 if (stwa.dirExcludeFilterPatternList.size()!=0) {
                     for (int i = 0; i < stwa.dirExcludeFilterPatternList.size(); i++) {
                         Pattern filter_pattern = stwa.dirExcludeFilterPatternList.get(i);
-                        Matcher mt = filter_pattern.matcher(filter_dir);
+                        String new_filter_dir = filter_dir;
+                        if (filter_pattern.toString().startsWith("(^/") || filter_pattern.toString().startsWith("(.*/")) {  // filter==*/cache/* -> excludes master_dir/cache/ and master_dir/subdir/cache/
+                            if (!filter_dir.startsWith("/")) new_filter_dir = "/" + filter_dir;                             // filter==/cache/ or filter==cache/, only excludes master_dir/cache/ while master_dir/subdir/cache/
+                        }                                                                                                   // filter==/cache equals /cache* !! and will match /cahemir for example
+                        if (!filter_pattern.toString().endsWith("/)") && !filter_pattern.toString().endsWith(".*)")) {     // fix above case: now filter==/cache -> exclude master/cache/*
+                            String new_pattern_str = filter_pattern.toString();
+                            stwa.util.addDebugMsg(2, "I", "old pattern= " + new_pattern_str);
+                            new_pattern_str = new_pattern_str.substring(0, new_pattern_str.length() - 1) + "/)";
+                            filter_pattern = Pattern.compile(new_pattern_str, Pattern.CASE_INSENSITIVE | Pattern.MULTILINE);
+                        }
+                        if (stwa.gp.settingDebugLevel >= 2)
+                            stwa.util.addDebugMsg(2, "I", "exclude dir filter_pattern=" + filter_pattern + " filter_dir=" + filter_dir + " new_filter_dir=" + new_filter_dir);
+                        Matcher mt = filter_pattern.matcher(new_filter_dir);
                         if (mt.find()) {
                             if (stwa.currentSTI.isSyncOptionUseExtendedDirectoryFilter1()) {
                                 if (matched_inc_array != null) {
