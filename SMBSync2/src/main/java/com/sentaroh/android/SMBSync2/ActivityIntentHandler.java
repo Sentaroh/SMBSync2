2311973a8f5b38bf2727d7f1f2d316532c488fe2
Sentaroh <gm.developer.fhoshino@gmail.com>
2020-06-10 12:46:59 +0900

    Show error info.

--- SMBSync2/src/main/java/com/sentaroh/android/SMBSync2/ActivityIntentHandler.java
+++ SMBSync2/src/main/java/com/sentaroh/android/SMBSync2/ActivityIntentHandler.java
@@ -1,10 +1,15 @@
 package com.sentaroh.android.SMBSync2;
 
 import android.app.Activity;
+import android.app.FragmentManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Window;
+
+import com.sentaroh.android.Utilities.Dialog.MessageDialogAppFragment;
+import com.sentaroh.android.Utilities.Dialog.MessageDialogFragment;
+import com.sentaroh.android.Utilities.NotifyEvent;
 
 public class ActivityIntentHandler extends Activity {
     @Override
@@ -17,16 +22,36 @@ public class ActivityIntentHandler extends Activity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_transrucent);
-
+        final Context c=ActivityIntentHandler.this;
         Intent received_intent=getIntent();
         if (received_intent.getAction()!=null && !received_intent.getAction().equals("")) {
             Intent in=new Intent(received_intent.getAction());
             in.setClass(ActivityIntentHandler.this, SyncService.class);
             if (received_intent.getExtras() != null) in.putExtras(received_intent.getExtras());
-            startService(in);
-        }
+            final FragmentManager fm=getFragmentManager();
+            try {
+                startService(in);
+                finish();
+            }catch(Exception e){
+                e.printStackTrace();
+                NotifyEvent ntfy=new NotifyEvent(c);
+                ntfy.setListener(new NotifyEvent.NotifyEventListener() {
+                    @Override
+                    public void positiveResponse(Context context, Object[] objects) {
+                        finish();
+                    }
 
-        finish();
+                    @Override
+                    public void negativeResponse(Context context, Object[] objects) {
+
+                    }
+                });
+                MessageDialogAppFragment mdf=MessageDialogAppFragment.newInstance(false, "E",
+                        "SMBSync2", "ActivityIntentHandler start service error\n"+e.getMessage());
+                mdf.showDialog(fm, mdf, ntfy);
+            }
+
+        }
     }
 
 }
